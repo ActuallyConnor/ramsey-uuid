6c0ed07efac7de1e6fd070184dbfe6fd60661618
connor <connorsmyth@rogers.com>
2022-02-21 13:49:26 -0500

    Attempting to use babel for tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,13 @@
-import type {Config} from '@jest/types';
+import type { Config } from '@jest/types';
+
 const config: Config.InitialOptions = {
   verbose: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
   coverageReporters: [
-    "lcov",
-    "text",
-    "json-summary"
-  ]
+    'lcov',
+    'text',
+    'json-summary',
+  ],
 };
 export default config;
