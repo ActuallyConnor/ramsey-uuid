3b38026e40763bbdb46620d9a8ae1519249c62e6
connor <connorsmyth@rogers.com>
2022-03-02 15:48:31 -0500

    Updating docs template

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -7,10 +7,11 @@ cd "$ROOT" || exit 1
 PATH="$(npm bin):$PATH"
 BUILD_DIR="$ROOT/build"
 DOCS_DIR="$ROOT/docs"
+DOCDASH_DIR="$ROOT/node_modules/docdash"
 
 # Clean up docs dir
 rm -rf "$DOCS_DIR"
 mkdir "$DOCS_DIR"
 
 # Compile docs
-jsdoc -d docs "$BUILD_DIR"
+jsdoc -d docs "$BUILD_DIR" -t "$DOCDASH_DIR"
