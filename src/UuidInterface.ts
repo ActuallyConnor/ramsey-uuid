23e41ac1daa1dbf6a250c58480d3d0a4cca629f6
Connor Smyth <connorsmyth@rogers.com>
2022-03-02 13:40:29 -0500

    Adding jsdocs (#23)

--- src/UuidInterface.ts
+++ src/UuidInterface.ts
@@ -1,10 +1,34 @@
+/**
+ * A UUID is a universally unique identifier adhering to an agreed-upon representation format and standard
+ * for generation
+ */
 export interface UuidInterface
 {
+  /**
+   * Returns the Array-like collection of 16 values (starting from offset) between 0-255
+   *
+   * @return ArrayLike<number>
+   */
   getUuid (): ArrayLike<number>;
 
+  /**
+   * Returns the string standard representation of the UUID
+   *
+   * @return string
+   */
   toString (): string;
 
+  /**
+   * Returns the hexadecimal representation of the UUID
+   *
+   * @return string
+   */
   toHex (): string;
 
+  /**
+   * Returns the binary string representation of the UUID
+   *
+   * @return string
+   */
   getBytes (): string;
 }
