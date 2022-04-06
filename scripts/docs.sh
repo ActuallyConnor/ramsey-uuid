2495ef8c54bc22271c5ada727ef4e807d3363a6b
Connor Smyth <connorsmyth@rogers.com>
2022-04-06 17:15:38 -0400

    Update dependencies (#28)
    * Update dependencies
    
    * Removing build dir
    
    * Building docs

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -5,9 +5,7 @@ ROOT="$(pwd)/$(dirname "$0")/.."
 cd "$ROOT" || exit 1
 
 PATH="$(npm bin):$PATH"
-BUILD_DIR="$ROOT/build"
 DOCS_DIR="$ROOT/docs"
-DOCDASH_DIR="$ROOT/node_modules/docdash"
 
 # Clean up docs dir
 rm -rf "$DOCS_DIR"
